ef5ab2976f4a366e4cf757aa6601a472985ee568
John Yorke <johnyorke@gmail.com>
2017-03-07 11:05:00 +0000

    Allow simultaneous gestures

--- ZoomRotatePanImageView.m
+++ ZoomRotatePanImageView.m
@@ -143,7 +143,7 @@
 #pragma mark - UIGestureRecognizer Delegate Methods
 
 - (BOOL) gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
-    return FALSE;
+    return TRUE;
 }
 
 #pragma mark - ZoomRotatePanImageViewDelegate
